a2ff0f8b6d0eba48f3ef90ee8869e34422d05772
Nguyen Duy <nguyenduy040510@gmail.com>
2024-08-31 01:03:57 +0700

    fixed keyframeExtractor indexing bug, refactored some of the codes

--- faissSearcher.py
+++ faissSearcher.py
@@ -1,16 +1,23 @@
 import faiss
 import queryEncoder
+import config
+import numpy as np
 
-def write_faiss_index(clip_embeddings, destination):
-    d = clip_embeddings.shape[1]
-    index = faiss.IndexFlatL2(d)
-    index.add(clip_embeddings)
-    faiss.write_index(index, destination)
+class faissSearcher:
+    def __init__(self):
+        pass
+    
+    def write_faiss_index(self, combined_clip_embeddings = np.load(config.combined_clip_file), destination = config.faiss_index_dir):
+        d = combined_clip_embeddings.shape[1]
+        index = faiss.IndexFlatL2(d)
+        index.add(combined_clip_embeddings)
+        faiss.write_index(index, destination)
 
-def read_faiss_index(faiss_index_dir):
-    return faiss.read_index(faiss_index_dir)
+    def read_faiss_index(self, faiss_index_dir=config.faiss_index_dir):
+        return faiss.read_index(faiss_index_dir)
 
-def search_frames(faiss_index, query, top_k=5):
-    text_embedding = queryEncoder.text_to_embedding(query)
-    D, I = faiss_index.search(text_embedding, top_k)  # `D` is distances, `I` is indices of nearest neighbors
-    return D, I
+    def search_frames(self, query, top_k=5):
+        faiss_index = self.read_faiss_index(query)
+        text_embedding = queryEncoder.text_to_embedding(query)
+        D, I = faiss_index.search(text_embedding, top_k)  # `D` is distances, `I` is indices of nearest neighbors
+        return D, I
--- main.py
+++ main.py
@@ -3,35 +3,21 @@ import numpy as np
 import os
 import torch
 import clip
-import clipEmbedding
 import clipIndex
+import Mapper
 
 
-# Load CLIP embeddings
-clip_dir = "./data/clip-features-vit-b32-sample"
-
-clip_embeddings = clipEmbedding.combine_clip_embeddings(clip_dir)
-clipEmbedding.save_clip_embeddings(clip_embeddings, "./data/clip-embeddings.npy")
-
-# Save CLIP indexes
-keyframe_map_dir = './data/map-keyframes'
-clip_index_destination = './data/clip-indexes.csv'
-
-clipIndex.save_clip_indexes(keyframe_map_dir, clip_index_destination)
-
-# Index FAISS
-faiss_index_dir = './data/faiss-index'
-
-faissSearcher.write_faiss_index(clip_embeddings, faiss_index_dir)
-faiss_index = faissSearcher.read_faiss_index(faiss_index_dir)
-
-# Search for similar frames
+# SEARCH QUERY GOES HERE!
 query = "Diver underwater hitting coral reef with hammer"
-result_distances, result_indices = faissSearcher.search_frames(faiss_index, query, top_k=5)
+
+result_distances, result_indices = faissSearcher.search_frames(query, top_k=5)
 print(result_indices)
 
 # Retrieve and display the results
 for indexes in result_indices:
     for idx in indexes:
-        result = clipIndex.look_up_clip_index(clip_index_destination, idx)
+        result = clipIndex.look_up_clip_index(idx)
         print(result)
+        frame_idx = result['frame_idx'].astype('int32')
+        # timestamp = Mapper.frame_index_to_timestamp(frame_idx)
+        # print(f"Frame index: {frame_idx}     Timestamp: {timestamp}")

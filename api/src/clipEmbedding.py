5903a71c6de34577be803a460ba0a9196a91361b
Nguyen Duy <nguyenduy040510@gmail.com>
2024-08-31 15:48:02 +0700

    optimized KeyframeExtractor

--- api/src/clipEmbedding.py
+++ api/src/clipEmbedding.py
@@ -3,28 +3,31 @@ import numpy as np
 import os
 import torch
 import clip
+import config
 
-def combine_clip_embeddings(clip_dir):
-
-    clip_files = []
-    for index in range(1, 32):
-        file_number = str(index).zfill(3)
-        filename = f"L01_V{file_number}.npy"
-        clip_files.append(os.path.join(clip_dir, filename))
-
-    # Initialize an empty list to store the features
-    clip_embeddings = []
-
-    # Loop through all .npy files in the directory
-    for clip_file in clip_files:
-        features = np.load(clip_file)
-        clip_embeddings.append(features)
-
-    # Concatenate all features into a single array
-    clip_embeddings = np.concatenate(clip_embeddings)
+class clipEmbedding:
+    def __init__(self):
+        pass
     
-    return clip_embeddings
+    def combine_clip_embeddings(self, clip_dir):
+        clip_files = []
+        for index in range(1, 32):
+            file_number = str(index).zfill(3)
+            filename = f"L01_V{file_number}.npy"
+            clip_files.append(os.path.join(clip_dir, filename))
 
+        # Initialize an empty list to store the features
+        clip_embeddings = []
 
-def save_clip_embeddings(clip_embeddings, destination):
-    np.save(destination, clip_embeddings)
+        # Loop through all .npy files in the directory
+        for clip_file in clip_files:
+            features = np.load(clip_file)
+            clip_embeddings.append(features)
+
+        # Concatenate all features into a single array
+        clip_embeddings = np.concatenate(clip_embeddings)
+        
+        return clip_embeddings
+
+    def save_clip_embeddings(clip_embeddings, destination):
+        np.save(destination, clip_embeddings)
--- api/src/main.py
+++ api/src/main.py
@@ -4,38 +4,22 @@ import os
 import torch
 import clip
 import clipIndex
+import queryEncoder
 import Mapper
-import clipEmbedding
-import config
-import clipIndex
-
-# Load CLIP embeddings
-clip_dir = "./data/clip-features-vit-b32-sample"
-clip_destination = "./out/clip-embeddings.npy"
-
-clip_embeddings = clipEmbedding.combine_clip_embeddings(clip_dir)
-clipEmbedding.save_clip_embeddings(clip_embeddings, clip_destination)
-
-# Save CLIP indexes
-keyframe_map_dir = './data/map-keyframes'
-clip_index_destination = './out/clip-indexes.csv'
-
-clipIndex = clipIndex.clipIndex()
-clipIndex.look_up_clip_index(keyframe_map_dir, clip_index_destination)
-
-# Index FAISS
-faiss_index_dir = './out/faiss-index'
-
-faissSearcher = faissSearcher.faissSearcher()
-faissSearcher.write_faiss_index(clip_embeddings, faiss_index_dir)
-faiss_index = faissSearcher.read_faiss_index(faiss_index_dir)
-
-# Search for similar frames
+from config import *
 
 # SEARCH QUERY GOES HERE!
-query = "Diver underwater hitting coral reef with hammer"
+query = "female reporter"
 
-result_distances, result_indices = faissSearcher.search_frames(query, top_k=5)
+queryEncoder = queryEncoder.queryEncoder()  # Initialize the query encoder
+encoded_query = queryEncoder.text_to_embedding(query)  # Encode the query
+
+faissSearcher = faissSearcher.faissSearcher() # Initialize the faiss searcher
+clipIndex = clipIndex.clipIndex() # Initialize the clip index
+
+faissSearcher.write_faiss_index() # Write the faiss index
+result_distances, result_indices = faissSearcher.search_frames(encoded_query, top_k=5)
+
 print(result_indices)
 
 # Retrieve and display the results

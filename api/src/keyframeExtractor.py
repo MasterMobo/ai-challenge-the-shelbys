0d7bb27da36e38100bacd80f903d72b9d1cbe6c5
Nguyen Duy <nguyenduy040510@gmail.com>
2024-09-04 18:13:39 +0700

    added middle keyframe info

--- api/src/keyframeExtractor.py
+++ api/src/keyframeExtractor.py
@@ -1,7 +1,9 @@
 import subprocess
-import os
+import os, shutil
 import csv
+import pandas as pd
 from pathlib import Path
+import math
 
 class KeyframeExtractor:
     data_dir = './data/videos'
@@ -38,10 +40,11 @@ class KeyframeExtractor:
         os.makedirs(self.keyframe_output_path, exist_ok=True)
         os.makedirs(self.metadata_output_path, exist_ok=True)
         
+        self.current_clip_index = 0
+        
         self.detectShots()
         self.extractShotData()
-
-        self.current_clip_index = 0
+        self.getMiddleFrame()
 
         print(f"Shot detection completed. Keyframes saved at {self.keyframe_output_path}. Metadata saved at {self.metadata_output_path}.")
 
@@ -49,7 +52,6 @@ class KeyframeExtractor:
         # Detect shots in a video and save keyframes and metadata
         # Save keyframes (as PNG's) into out/keyframes
         # Save metadata into out/metadata
-
         folder_paths = self.data_path.iterdir()
 
         for folder_path in folder_paths:
@@ -82,7 +84,47 @@ class KeyframeExtractor:
                 with open(metadata_file, 'w') as f:
                     subprocess.run(command, stderr=f)
 
-                   
+    def getMiddleFrame(self):
+        """
+        This func will calculate the frame index i of the middle frame of a certain shot
+        """
+        metadata_processed_df = pd.read_csv(self.metadata_processed_path)
+        for i in range(len(metadata_processed_df) - 1):
+
+            cur_keyframe_index = metadata_processed_df.iloc[i]['frame_index']
+            cur_keyframe_timestamp = metadata_processed_df.iloc[i]['timestamp']
+            cur_keyframe_pts = metadata_processed_df.iloc[i]['pts_time']
+
+            next_keyframe_index = metadata_processed_df.iloc[i+1]['frame_index']
+
+            # then calculate the frame index based on the timestamp
+            middle_frame_index = int((cur_keyframe_index + next_keyframe_index) // 2)
+            middle_frame_pts = middle_frame_index / 25 #float
+            # hh:mm:ss
+            middle_frame_timestamp = f"{int(middle_frame_index // 3600)}:{int((middle_frame_index % 3600) // 60)}:{round(middle_frame_index % 60, 2)}"
+            
+            # write result to mtdt csv 
+            metadata_processed_df.at[i, 'middle_index'] = middle_frame_index
+            metadata_processed_df.at[i, 'middle_timestamp'] = middle_frame_timestamp
+            metadata_processed_df.at[i, 'middle_pts'] = middle_frame_pts
+
+            # current iter if the last shot is reached
+            if (next_keyframe_index < cur_keyframe_index):
+                metadata_processed_df.at[i, 'middle_index'] = cur_keyframe_index
+                metadata_processed_df.at[i, 'middle_timestamp'] = cur_keyframe_timestamp
+                metadata_processed_df.at[i, 'middle_pts'] = cur_keyframe_pts
+
+        # Handle the last shot
+        metadata_processed_df.at[len(metadata_processed_df) - 1, 'middle_index'] = metadata_processed_df.iloc[-1]['frame_index']
+        metadata_processed_df.at[len(metadata_processed_df) - 1, 'middle_timestamp'] = metadata_processed_df.iloc[-1]['timestamp']
+        metadata_processed_df.at[len(metadata_processed_df) - 1, 'middle_pts'] = metadata_processed_df.iloc[-1]['pts_time']
+        
+        # convert middle_index to int
+        metadata_processed_df['middle_index'] = metadata_processed_df['middle_index'].astype(int)
+
+        # save to .csv
+        metadata_processed_df.to_csv(self.metadata_processed_path, index=False)
+
     def extractShotData(self):
         # Read metadata files and write to human-readable csv
         
@@ -104,11 +146,9 @@ class KeyframeExtractor:
             keyframe_images = sorted(keyframe_folder.iterdir(),
                                     key=lambda x: self.getFrameIndex(x.stem))
             
-
             # Read from metadata and write to .csv file
             with open(metadata_file, 'r') as f, open(self.metadata_processed_path, 'a', newline='') as out_f:
-                csv_writer = csv.writer(out_f)
-                
+                csv_writer = csv.writer(out_f)                
                 pts_time_arr = []
 
                 # Find and append pts_time to pts_time_arr
@@ -139,9 +179,7 @@ class KeyframeExtractor:
 
     def getFrameIndex(self, videoName: str):
         return int(videoName.split("_")[-1])
-
-    
-
+            
 if (__name__ == "__main__"):
     # Usage example
     detector = KeyframeExtractor(scene_threshold=0.3)

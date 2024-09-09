02a4c98a6124e5f4b9b93cb70dc9a09df5a1b01a
Khưu Hải Châu <haichaukhuu@gmail.com>
2024-09-09 14:32:34 +0700

    add out file name param

--- api/src/main.py
+++ api/src/main.py
@@ -9,9 +9,13 @@ app = Flask(__name__)
 @app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('query')
+    output_file_name = request.args.get('out')
 
+    if (query is None) or (output_file_name is None):
+        return jsonify({"error": "Missing 'query' or 'out' parameter"}), 400
+    
     result_df = videoSearcher.search(query)
-    videoSearcher.writeResult.write_to_csv(result_df)
+    videoSearcher.writeResult.write_to_csv(result_df, output_file_name)
     result_json = result_df.to_dict(orient='records')
 
     return jsonify(result_json)
--- api/src/writeResult.py
+++ api/src/writeResult.py
@@ -20,7 +20,7 @@ class WriteResult:
         if not os.path.exists(self.check_res_dir):
             os.makedirs(self.check_res_dir, exist_ok=True)
 
-    def write_to_csv(self, result_df: pd.DataFrame):
+    def write_to_csv(self, result_df: pd.DataFrame, output_file_name:str):
         # Write the results to a csv file
 
         # extract the video name and frame index
@@ -28,15 +28,16 @@ class WriteResult:
 
         # Define the output file path
 
-        time_of_query = datetime.now()
-        hour = time_of_query.hour
-        minute = time_of_query.minute
-        second = time_of_query.second
-        day = time_of_query.day
-        month = time_of_query.month
-        year = time_of_query.year
+        # time_of_query = datetime.now()
+        # hour = time_of_query.hour
+        # minute = time_of_query.minute
+        # second = time_of_query.second
+        # day = time_of_query.day
+        # month = time_of_query.month
+        # year = time_of_query.year
 
-        submit_file = self.submit_path / f'{hour:02d}h{minute:02d}m{second:02d}_{day:02d}_{month:02d}_{year:04d}.csv'
+        # submit_file = self.submit_path / f'{hour:02d}h{minute:02d}m{second:02d}_{day:02d}_{month:02d}_{year:04d}.csv'
+        submit_file = self.submit_path / f'{output_file_name}.csv'
 
         # Write the results to a csv file
         answer_df.to_csv(submit_file, index=False, header=False)
@@ -46,7 +47,7 @@ class WriteResult:
 
         check_df = result_df[['video_name', 'frame_index', 'timestamp', 'middle_index','middle_timestamp']]
 
-        check_res_file =  self.check_res_path / f'{hour:02d}h{minute:02d}m{second:02d}_{day:02d}_{month:02d}_{year:04d}.csv'
+        check_res_file =  self.check_res_path / f'{output_file_name}_check.csv'
         check_df.to_csv(check_res_file, header=True)
         
         print(f"Please double check results in {check_res_file}")
